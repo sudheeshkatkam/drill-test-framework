f522b732b7ab1148cd597796689a22919889686a
Sudheesh Katkam <skatkam@maprtech.com>
2016-06-30 10:48:14 -0700

    Set and reset slice_target before and after run

--- before-run.sql
+++ before-run.sql
@@ -4,3 +4,4 @@ alter system set `planner.enable_decimal_data_type` = true;
 alter system set `new_view_default_permissions` = '777';
 alter system set `planner.enable_limit0_optimization` = true;
 alter system set `exec.errors.verbose` = true;
+alter system set `planner.slice_target` = 2;
